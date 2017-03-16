4108ed413b703140da9d99a062c3209306263521
MarcHines <mxh857@miami.edu>
2017-03-16 13:06:41 -0400

    Update Path-sum.java

--- Path-sum.java
+++ Path-sum.java
@@ -11,3 +11,25 @@ Given the below binary tree and sum = 22,
     
         return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
     }
+
+
+//The above solution works for Binary Search Trees, but what about Binary Tree's??
+
+//FOR BINARY TREES!!!!!
+ private List<List<Integer>> resultList = new ArrayList<List<Integer>>();
+    
+    public void pathSumInner(TreeNode root, int sum, Stack<Integer>path) {
+        path.push(root.val);
+        if(root.left == null && root.right == null)
+            if(sum == root.val) resultList.add(new ArrayList<Integer>(path));
+        if(root.left!=null) pathSumInner(root.left, sum-root.val, path);
+        if(root.right!=null)pathSumInner(root.right, sum-root.val, path);
+        path.pop();
+    }
+    
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        if(root==null) return resultList;
+        Stack<Integer> path = new Stack<Integer>();
+        pathSumInner(root, sum, path);
+        return resultList;
+    }
