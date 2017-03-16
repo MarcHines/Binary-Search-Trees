65d32887e8889f0ad0e904b6c9e1f096c5ff3d56
MarcHines <mxh857@miami.edu>
2017-03-16 13:23:45 -0400

    Create Count-leaves.java

---
+++ Count-leaves.java
@@ -0,0 +1,9 @@
+int countLeaves(Node node){
+  if( node == null )
+    return 0;
+  if( node.left == null && node.right == null ) {
+    return 1;
+  } else {
+    return countLeaves(node.left) + countLeaves(node.right);
+  }
+}
